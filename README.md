c6fb8195eb6d0aebf6419d069c51a8a441d16735
twairball <twairball@yahoo.com>
2017-03-29 17:21:01 +0800

    added preprocessor to conveniently grab datasets out of model

--- README.md
+++ README.md
@@ -17,3 +17,24 @@ Prototype RNN approach to predicting PM2.5 time series
 - Add column for time of day, day of week
 - Save as bcolz arrays
 
+
+### Useage
+
+- Supply pandas dataframe of features,  indexed by timeseries sampled by hour. 
+
+````
+    from models import *
+    
+    # create model with dataframe features, df, 
+    # supply labels with dataframe with single column, df[['pm25]]
+    base_model = BaseModel(df, df[['pm25']], look_back=3)
+
+    # train model 
+    base_model.train(nb_epoch=50)
+
+    # test model, this will print RMSE errors
+    testPredict = base_model.test()
+
+    # get training and testing labels for measurement
+    train_labels, test_labels = base_model.get_labels()
+````
--- models.py
+++ models.py
@@ -36,6 +36,42 @@ class Dataset():
         dataset = self.normalized_values()
         return split_datasets(dataset, self.ratio)
 
+class DatasetPreprocessor():
+    def __init__(self, df_feats, df_labels, look_back=3):
+        self.look_back = look_back
+        # features dataset
+        self.ds_feats = Dataset(df_feats)        
+        # labels dataset
+        self.ds_labels = Dataset(df_labels)
+
+    def get_datasets(self):
+        ### TODO: refactor
+        # get train set
+        train, test = self.ds_feats.split_datasets()
+        # get labels
+        train_labels, test_labels = self.ds_labels.split_datasets()
+
+        # create datasets with lookback
+        train_feats, train_labels = lookback_dataset(train, train_labels, self.look_back)
+        test_feats, test_labels = lookback_dataset(test, test_labels, self.look_back)
+
+        return train_feats, train_labels, test_feats, test_labels
+    
+    def label_scale(self):
+        return self.ds_labels.df_scales[0]
+
+    def get_labels(self):
+        """
+        Returns (un-normlized) labels
+        """
+        train_feats, train_labels, test_feats, test_labels = self.get_datasets()
+        # revert scaling
+        train_labels = train_labels * self.label_scale()
+        test_labels = test_labels * self.label_scale()
+        return train_labels, test_labels
+
+
+
 class BaseModel():
     def __init__(self, df_feats, df_labels, look_back=3):
         self.look_back = look_back
