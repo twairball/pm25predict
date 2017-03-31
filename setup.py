1b87b66b56f6225b6f7f1d55e62b716b436eb0be
twairball <twairball@yahoo.com>
2017-03-31 17:25:14 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# list requirements for setuptools
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = []
+    for name in f.readlines():
+        if not name.startswith('--'):
+            requirements.append(name.rstrip())
+
+setup(
+    name='pm25predict',
+    version='0.1',
+    description='PM2.5predict',
+    author='twairball',
+    author_email='twairball@yahoo.com',
+    packages=find_packages(exclude=['contrib', 'docs', 'test*']),
+    install_requires=requirements,
+)
